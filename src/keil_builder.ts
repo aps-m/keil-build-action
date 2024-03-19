6bdda3b7bc056aea6ebcb19d542f9cdb5deb750f
Sergey Ladanov <sergei_ladanov_93@mail.ru>
2024-03-19 13:34:11 +0300

    add

--- src/keil_builder.ts
+++ src/keil_builder.ts
@@ -1,4 +1,4 @@
-const KeilCompilerPath = 'Compiler=C:\\Keil_v5\\UV4\\UV4.exe'
+const KeilCompilerPath = 'C:\\Keil_v5\\UV4\\UV4.exe'
 
 function CallBack(err: string, data: string, stderr: string): void {
   if (err) {
@@ -19,7 +19,7 @@ export function KeilBuildProject(
   target_name: string
 ): void {
   const cmdShell = require('node-cmd')
-
+  
   let process_obj = cmdShell.run(
     `${KeilCompilerPath} -j0 -cr ${project_name} -t ${target_name}`,
     CallBack
