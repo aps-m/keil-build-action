b3e563ebfcb1aaf1b707d20a54c64398726cd77e
Sergey Ladanov <sergei_ladanov_93@mail.ru>
2024-03-19 13:36:15 +0300

    add

--- src/keil_builder.ts
+++ src/keil_builder.ts
@@ -21,7 +21,7 @@ export function KeilBuildProject(
   const cmdShell = require('node-cmd')
   
   let process_obj = cmdShell.run(
-    `${KeilCompilerPath} -j0 -cr ${project_name} -t ${target_name}`,
+    `start ${KeilCompilerPath} -j0 -cr ${project_name} -t ${target_name}`,
     CallBack
   )
 
